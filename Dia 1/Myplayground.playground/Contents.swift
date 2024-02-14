aa37e1fe8eb98edc6b1b209e5028f625bed8cc61
Nestor <neivertjosias@gmail.com>
2024-02-14 19:25:09 -0400

    initial commit

---
+++ Dia 1/Myplayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+
+//: Variables
+
+var greeting = "Hello, playground"
+str = "Good bye"
+
+
+//: String and integers
+
+var str = "Hola mundo"
+var age = 38
+var population = 8_000_000
+
+
+//: Multiline Strings
+
+var str1 = """
+this Goes
+over multiple
+lines
+"""
+
+var str2 = """
+this over \
+over multiple \
+lines
+"""
+
+//: doubles and booleans
+var pi = 3.141
+var awesome = true
+
+//: Strings interpolations
+var score = 85
+var str3 = "Your Score was \(score)"
+var result = "The test result are here: \(str3)"
+
+//: Constants
+
+let taylor = "Swift"
+
+//: Tyoe Annotations
+
+let str4 = "Hello playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRock: Bool = true
+
